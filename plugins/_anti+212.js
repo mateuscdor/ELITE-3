9daa151e9734fedbb8a282be4de45cd4b7a35107
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-06 17:29:24 -0500

    Update _anti+212.js

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -1,18 +1,18 @@
 let handler = m => m
 
 handler.before = async function (m) {
-   if (m.sender.startsWith('212' || '212')) {
+   if (m.sender.startsWith('212' || '234')) {
    	global.db.data.users[m.sender].banned = false
 conn.reply('AntiMoros Activado lo siento mirey seras expulsado del grupo.😄')
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    }
    
-   if (m.sender.startsWith('265' || '265')) {
+   if (m.sender.startsWith('265' || '380')) {
    	global.db.data.users[m.sender].banned = true
 conn.reply('AntiMoros Activado lo siento mirey seras expulsado del grupo.😄')
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
-if (m.sender.startsWith('92' || '92')) {
+if (m.sender.startsWith('62' || '92')) {
    	global.db.data.users[m.sender].banned = true
 conn.reply('AntiMoros Activado lo siento mirey seras expulsado del grupo.😄')
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
