eb36a49dd6c0d2d98a0763fcd4c0315aa2e668c7
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-06 23:49:10 -0500

    Update gc-unbanchat.js

--- plugins/gc-unbanchat.js
+++ plugins/gc-unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
-  if (!(m.chat in global.db.data.chats)) return m.reply('*Este chat no está registrado en la base de datos!*')
+  if (!(m.chat in global.db.data.chats)) return m.reply('              *[❗𝐈𝐍𝐅𝐎❗]* \n\n*𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙽𝙾 𝙴𝚂𝚃𝙰 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾 𝙴𝙽 𝙻𝙰 𝙱𝙰𝚂𝙴 𝙳𝙴 𝙳𝙰𝚃𝙾𝚂.*')
   let chat = global.db.data.chats[m.chat]
-  if (!chat.isBanned) return m.reply('*Este chat no está baneado!!*')
+  if (!chat.isBanned) return m.reply('              *[❗𝐈𝐍𝐅𝐎❗]* \n\n*𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙽𝙾 𝙴𝚂𝚃𝙰 𝙱𝙰𝙽𝙴𝙰𝙳𝙾.*')
   chat.isBanned = false
-  m.reply('*✅ Listo... chat desbaneado!*')
+  m.reply('                       *[❗𝐈𝐍𝐅𝐎❗]* \n\n*𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙵𝚄𝙴 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾.*')
 }
 handler.command = /^unbanchat$/i
 handler.admin = true
