5cf8c60f57d59158d7123e05936fd136fbe57790
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-07 14:56:05 -0500

    Update resetprefixbot.js

--- plugins/resetprefixbot.js
+++ plugins/resetprefixbot.js
@@ -1,6 +1,7 @@
 let handler = async(m, { conn }) => {
 
   global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+     //await m.reply(`Prefix berhasil direset`)
      conn.fakeReply(m.chat, '*❗Prefijo restablecido con éxito❗*', '0@s.whatsapp.net', '•𝐑𝐞𝐬𝐞𝐭 𝐩𝐫𝐞𝐟𝐢𝐱•')
 }
 handler.help = ['resetprefix']
