ef93e226211a53920a88a25b1e1717358eda273e
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-07 17:21:48 -0500

    Update gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -5,7 +5,7 @@ conn.reply(m.chat, '*🤖 Aqui tienes el link del grupo:*\n\n 🔰 https://chat.
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '    ❗ 𝐋𝐈𝐍𝐊 𝐃𝐄𝐋 𝐆𝐑𝐔𝐏𝐎 ❗',
 body: 'ڪے(̅_̅_̅(̲̲̲̲̲̅̅̅̅̅̅(̅_̅_̲̅✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹 _̅_̅_̅()ڪے',         
-previewType: 0, thumbnail: fs.readFileSync("./Cargando.jpg"),
+previewType: 0, thumbnail: fs.readFileSync("./Menulink.jpg"),
 sourceUrl: null}}})   
 }
 handler.help = ['linkgroup']
