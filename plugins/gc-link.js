57953f806ac938a2653f1a3f126a2adca4b9ecd1
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-07 17:32:39 -0500

    Update gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -5,7 +5,7 @@ conn.reply(m.chat, 'Aqui tienes el link del grupo\n\nhttps://chat.whatsapp.com/'
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
 body: '𝖀𝖓𝕻𝖙𝖔𝕬𝖉𝖗𝖎𝖍𝕭𝖔𝖙',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+previewType: 0, thumbnail: fs.readFileSync("./Cargando.jpg"),
 sourceUrl: null}}})   
 }
 handler.help = ['linkgroup']
