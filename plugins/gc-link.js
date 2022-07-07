2dc6c7382673d58228a3a53d7d251fb904cef565
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-07 17:44:04 -0500

    Update gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -1,11 +1,11 @@
 import fs from 'fs'
 let handler = async (m, { conn, args }) => {
 let group = m.chat
-conn.reply(m.chat, '*🤖Aqui tienes el link del grupo*\n\n🔰https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
+conn.reply(m.chat, '*🤖 Aqui tienes el link del grupo:*\n\n 🔰 https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '    ❗ 𝐋𝐈𝐍𝐊 𝐃𝐄𝐋 𝐆𝐑𝐔𝐏𝐎 ❗',
-body: 'ڪے(̅_̅_̅(̲̲̲̲̲̅̅̅̅̅̅(̅_̅_̲̅✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹 _̅_̅_̅()ڪے'',         
-previewType: 0, thumbnail: fs.readFileSync("./Cargando.jpg"),
+body: 'ڪے(̅_̅_̅(̲̲̲̲̲̅̅̅̅̅̅(̅_̅_̲̅✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹 _̅_̅_̅()ڪے',         
+previewType: 0, thumbnail: fs.readFileSync("./Menulink.jpg"),
 sourceUrl: null}}})   
 }
 handler.help = ['linkgroup']
@@ -13,4 +13,5 @@ handler.tags = ['group']
 handler.command = /^(link)$/i
 handler.group = true
 handler.botAdmin = true
+handler.premium = true
 export default handler
