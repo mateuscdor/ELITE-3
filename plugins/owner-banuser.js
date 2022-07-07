e7f8d388770cdae4ee974989e04fcc85faea7b0d
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-07 10:05:48 -0500

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,18 +1,17 @@
-let handler = async (m, { conn, text}) => {
-    if (!text) throw '*_A QUIEN QUIERES BANEAR?_*'
-    let who
-    if (m.isGroup) who = m.mentionedJid[0]
-    else who = m.chat
-    if (!who) throw '*_ETIQUETA A ALGUN USUARIO_*'
-    let users = global.db.data.users
-    users[who].banned = true
-    conn.reply(m.chat, `*_EL USUARIO FUE BANEADO CON EXITO_*\n\n*_EL USUARIO NO TENDRA PERMISO PARA USAR EL BOT_*`, m)
-}
-handler.help = ['banuser']
-handler.tags = ['General']
-handler.command = /^(ban)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = true
+let handler = async (m, { conn, text, command, usedPrefix }) => {
+if (!text) throw `${mg}  ❗𝙀𝙏𝙄𝙌𝙐𝗘𝗧𝘼𝗥 𝗔 𝘼𝙇𝙂𝙐𝙄𝙀𝙉 𝙋𝘼𝙍𝘼 𝘽𝘼𝙉𝙀𝘼𝙍.\n|n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} @tag*`
+let who
+if (m.isGroup) who = m.mentionedJid[0]
+else who = m.chat
+if (!who) throw `${mg} ❗𝗘𝗧𝗜𝗤𝗨𝗘𝗧𝗔𝗥 𝗔 𝗔𝗟𝗚𝗨𝗜𝗘𝗡 𝗣𝗔𝗥𝗔 𝗕𝗔𝗡𝗘𝗔𝗥.\n\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} @tag*`
+let users = global.db.data.users
 
+let username = conn.getName(who)
+conn.reply(m.chat, `${eg}    ❗ 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗕𝗔𝗡𝗘𝗔𝗗𝗢 ❗\n\n➖Registrado en blocklist .\n➖No accede a comandos .\n➖BOT ignora a :\n      ${username}. \n\n*𝙰𝚃𝚃𝙴: 𝗕𝗢𝗧 ${gt}*`, m)
+}
+handler.help = ['ban']
+handler.tags = ['owner']
+handler.command = /^ban$/i
+handler.group = true
+handler.admin = true 
 export default handler
