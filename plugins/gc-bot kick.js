7fb1556ea432bfc10574be2f9d595d7de127988c
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-07 15:56:03 -0500

    Update gc-bot kick.js

--- plugins/gc-bot kick.js
+++ plugins/gc-bot kick.js
@@ -1,24 +1,22 @@
 import { areJidsSameUser } from '@adiwajshing/baileys'
 let handler = async (m, { conn, participants }) => {
-    let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
-    let kickedUser = []
-    for (let user of users)
-        if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin) {
-            const res = await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-            kickedUser.concat(res)
-            delay(1 * 1000)
+let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
+let kickedUser = []
+for (let user of users)
+if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin) {
+if (!global.db.data.settings[conn.user.jid].restrict) throw '*[ ⚠️ ] 𝙴𝙻 𝙾𝚆𝙽𝙴𝚁 𝚃𝙸𝙴𝙽𝙴 𝚁𝙴𝚂𝚃𝚁𝙸𝙽𝙶𝙸𝙳𝙾 (𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝 / 𝚍𝚒𝚜𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) 𝙴𝙻 𝚄𝚂𝙾 𝙳𝙴 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾*'
+const res = await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+kickedUser.concat(res)
+await delay(1 * 1000)
         }
-    m.reply(`✅A sus ordenes mirey, la rata fue expulsada ${kickedUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: kickedUser })
+    m.reply(`*❗USUARIO EXPULSADO❗* ${kickedUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: kickedUser })
 
 }
 handler.help = ['kick @user']
 handler.tags = ['group']
-handler.customPrefix = /bot kick|bot expulsalo|bot echalo|bot échalo/i
-handler.command = new RegExp
-handler.admin = true
+handler.command = /^(kick|sacar|\-)$/i
 handler.group = true
+handler.admin = true
 handler.botAdmin = true
-
 export default handler
-
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
