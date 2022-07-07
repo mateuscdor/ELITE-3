215917286929c43dde81d0be08dd0630d6fbd340
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-06 19:17:37 -0500

    Update adult-comandos+18.js

--- plugins/adult-comandos+18.js
+++ plugins/adult-comandos+18.js
@@ -120,4 +120,4 @@ handler.command = ['nsfwloli', 'nsfwfoot', 'nsfwass', 'nsfwbdsm', 'nsfwcum', 'ns
 handler.limit = true
 handler.admin = true
 handler.tags = ['nsfw']
-export default  handler/*
+export default  handler*/
