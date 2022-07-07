48961fe096bf336c8b75a78ce44dda9cca8a89be
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-07 11:24:33 -0500

    Update random-navidad.js

--- plugins/random-navidad.js
+++ plugins/random-navidad.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+/*import fetch from 'node-fetch'
 import axios from 'axios'
 let handler = async(m, { conn, args, usedPrefix, command }) => {
 let res = await axios("https://meme-api.herokuapp.com/gimme/Christmas")
@@ -10,3 +10,4 @@ handler.help = ['navidad']
 handler.tags = ['internet']
 handler.command = /^(navidad)$/i
 export default handler
+*/
