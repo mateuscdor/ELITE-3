e29a0152627d38d372c8d3c0009370e8b3a58687
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-08 01:23:14 -0500

    Update cc-peru.js

--- plugins/cc-peru.js
+++ plugins/cc-peru.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 await conn.sendMessage(m.chat, { react: { text: '🚀', key: m.key } })  
-
+await m.reply(global.wait)
 let text = `🚨 NIDOS   MUNDIALES 🚨*
  	📲 OSH	🇺🇸	❖	34.0152,-118.2869	
 🅖
