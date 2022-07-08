a94d0e9852777649e1840dc5f924f32202058a0e
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-08 01:44:05 -0500

    Update cc-peru.js

--- plugins/cc-peru.js
+++ plugins/cc-peru.js
@@ -3,16 +3,9 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 await conn.sendMessage(m.chat, { react: { text: '🚀', key: m.key } })  
 await m.reply(global.wait)
 let text = `hola
-`.trim()   
-conn.reply(m.chat, text, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: '𝐍𝐈𝐃𝐎𝐒.',
-body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
-previewType: 0, thumbnail: fs.readFileSync("./Cargando.jpg"),
-sourceUrl: `http://wa.me/+51996089079`}}}) 
-}
-let text = `123
 `.trim()
+let text = `123
+`.trim()   
 conn.reply(m.chat, text, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝐍𝐈𝐃𝐎𝐒.',
