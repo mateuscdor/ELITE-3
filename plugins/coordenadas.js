ba4cd05643502d3f37eb22dc471a6848f83bcaf9
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-08 01:06:36 -0500

    Update coordenadas.js

--- plugins/coordenadas.js
+++ plugins/coordenadas.js
@@ -3,7 +3,7 @@ import PhoneNumber from 'awesome-phonenumber'
 import { promises } from 'fs'
 import { join } from 'path'
 let handler = async (m, { conn, usedPrefix, command, args, usedPrefix: _p, __dirname, isOwner, text, isAdmin, isROwner }) => {
-  
+await conn.sendMessage(m.chat, { react: { text: '✨', key: m.key } })  
   
 const { levelling } = '../lib/levelling.js'
 //let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text }) => {
