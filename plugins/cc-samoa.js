bf00c578f66bb894681095f1913ab68773d6dfc2
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-08 08:06:43 -0500

    Create cc-samoa.js

---
+++ plugins/cc-samoa.js
@@ -0,0 +1,17 @@
+import fs from 'fs'
+let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+await conn.sendMessage(m.chat, { react: { text: '🚀', key: m.key } })  
+await m.reply(global.wait)
+let text = `*-14.277941,-170.688202*
+`.trim()   
+conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝐍𝐈𝐃𝐎𝐒.',
+body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
+previewType: 0, thumbnail: fs.readFileSync("./Cargando.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}}) 
+}
+handler.command = /^(samoa)/i
+handler.register = true
+handler.limit = 2
+export default handler
