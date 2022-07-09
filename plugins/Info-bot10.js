eff3de489dc84f3cc3cdacc3b67afcb8a5626cd9
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-09 12:34:27 -0500

    Create Info-bot10.js

---
+++ plugins/Info-bot10.js
@@ -0,0 +1,22 @@
+import util from 'util'
+import path from 'path'
+
+let handler = async (m, { conn}) => {
+await conn.sendMessage(m.chat, { react: { text: '✨', key: m.key } })
+let name = conn.getName(m.sender)
+
+let fgac = ["cris ofc", "andrea"]
+let nfg = fgac[Math.floor(Math.random() * fgac.length)] 
+
+conn.sendHydrated(m.chat, `*👤hola ${name}* \n\n *necesita ayuda *`, igfg, '', 'http://wa.me/51938 089515', '🤖', null, null, [
+      ['⦙☰ Menu', '.menu'],
+      ['⦙☰ Exp', '.xpgr'],
+    ], m)
+conn.sendFile(m.chat, null, m, true, {
+type: 'audioMessage', 
+ptt: true
+})
+}
+handler.customPrefix = /^(abz|robopt)$/i
+handler.command = new RegExp
+export default handler
