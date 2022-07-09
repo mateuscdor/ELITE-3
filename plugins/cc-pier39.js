0d14195cc64b73296427bdf02617cf31cb52a7e7
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-09 10:31:22 -0500

    Update cc-pier39.js

--- plugins/cc-pier39.js
+++ plugins/cc-pier39.js
@@ -7,9 +7,9 @@ let text = `*37.80867,-122.40982*
 `.trim()   
 conn.reply(m.chat, text, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: '𝐍𝐈𝐃𝐎𝐒.',
+title: '❗𝐂𝐨𝐨𝐫𝐝𝐞𝐧𝐚𝐝𝐚❗',
 body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
-previewType: 0, thumbnail: fs.readFileSync("./Cargando.jpg"),
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
 sourceUrl: `http://wa.me/+51996089079`}}}) 
 }
 handler.command = /^(pier39)/i
