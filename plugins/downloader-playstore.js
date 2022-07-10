bd0208653bd0f8e8ea9282ed2430d28f32f8bb05
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-10 09:57:42 -0500

    Update downloader-playstore.js

--- plugins/downloader-playstore.js
+++ plugins/downloader-playstore.js
@@ -1,27 +1,31 @@
-import cheerio from 'cheerio'
-import fetch from 'node-fetch'
+let fetch = require('node-fetch')
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-	if (!args[0]) throw `Ex: ${usedPrefix + command} https://play.google.com/store/apps/details?id=com.linecorp.LGGRTHN`
-	let res = await apkDl(args[0])
-	await m.reply('_In progress, please wait..._')
-	conn.sendMessage(m.chat, { document: { url: res.download }, mimetype: res.mimetype, fileName: res.fileName }, { quoted: m })
+let handler = async(m, { conn, text }) => {
+
+  if (!text) return conn.reply(m.chat, 'Harap Masukan nama aplikasi', m)
+
+  await m.reply('Searching...')
+    let res = await fetch(`https://caliphapi.com/api/playstore/search?apikey=ELYASXD&q=${text}`)
+    let json = await res.json()
+    if (res.status !== 200) throw await res.text()
+    if (!json.status) throw json
+    let thumb = json.result[0].icon
+    let hasil = `*── 「 𝐏𝐋𝐀𝐘 𝐒𝐓𝐎𝐑𝐄 」 ──*
+▢ *𝙽𝙾𝙼𝙱𝚁𝙴*: ${json.result[0].title}
+▢ *𝙰𝙿𝙿 𝙸𝙳*: ${json.result[0].appId}
+▢ *DEVELOPER*: ${json.result[0].developer}
+▢ *𝙴𝚂𝚃𝙰𝙳𝙾*: ${json.result[0].priceText}
+▢ *HARGA*: ${json.result[0].price}
+▢ *RATING*: ${json.result[0].scoreText}
+▢ *DESKRIPSI*: ${json.result[0].summary}
+▢ *𝚄𝚁𝙻*:  
+${json.result[0].url}
+`
+
+    conn.sendButtonImg(m.chat, thumb, hasil, wm, 'Pencet', 'huuu', m)
 }
-handler.help = handler.alias = ['apkdl']
-handler.tags = ['downloader']
+handler.help = ['playstore'].map(v => v + ' <nama apk>')
+handler.tags = ['internet']
 handler.command = /^(playstore1)$/i
-
-
-async function apkDl(url) {
-	let res = await fetch('https://apk.support/gapi/index.php', {
-		method: 'post',
-		body: new URLSearchParams(Object.entries({ x: 'downapk', t: 1, google_id: url, device_id: '', language: 'en-US', dpi: 480, gl: 'SUQ=', model: '', hl: 'en', de_av: '', aav: '', android_ver: 5.1, tbi: 'arm64-v8a', country: 0, gc: undefined }))
-	})
-	let $ = cheerio.load(await res.text())
-	let fileName = $('div.browser > div.dvContents > ul > li > a').text().trim().split(' ')[0]
-	let download = $('div.browser > div.dvContents > ul > li > a').attr('href')
-	if (!download) throw 'Can\'t download the apk!'
-	let mimetype = (await fetch(download, { method: 'head' })).headers.get('content-type')
-	return { fileName, mimetype, download }
-}
+handler.limit = true
 export default handler
