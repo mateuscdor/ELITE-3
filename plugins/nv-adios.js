bb4440bf73b21c4fa8ba84ae485d9f1ac13a1a22
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-10 13:27:57 -0500

    Update and rename sv_adios.js to nv-adios.js

--- plugins/nv-adios.js
+++ plugins/nv-adios.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/Adios.mp3'
+let vn = './media/media_Adios.mp3'
 conn.sendFile(m.chat, vn, 'Adios.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
