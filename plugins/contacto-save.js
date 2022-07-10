01200b885ad9c7b1c3c67e73b48a4b67f520af53
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-10 10:36:04 -0500

    Create contacto-save.js

---
+++ plugins/contacto-save.js
@@ -0,0 +1,14 @@
+let handler = async (m, { conn, text }) => {
+  if (!text) return
+  let who
+  if (m.isGroup) who = m.mentionedJid[0]
+  else who = m.chat
+  if (!who) throw 'etiquetas algunos de ellos'
+  txt = text.replace('@' + who.split`@`[0], '').trimStart()
+  return conn.sendContact(m.chat, who, txt || conn.getName(who), m)
+}
+handler.help = ['save'].map(v => v + ' @mention <ContactName>')
+handler.tags = ['']
+
+handler.command = /^save$/
+export default handler
