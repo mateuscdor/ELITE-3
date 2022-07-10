6bd001707785410c0e2083a11bb2bc8a96f1c856
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-09 21:02:05 -0500

    Create adios.js

---
+++ plugins/adios.js
@@ -0,0 +1,14 @@
+import util from 'util'
+import path from 'path'
+
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+let vn = './media/Adios.mp3'
+conn.sendFile(m.chat, vn, 'Adios.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /adios|bye|chau|𝙰𝙳𝙸𝙾𝚂/
+handler.command = new RegExp
+export default handler
