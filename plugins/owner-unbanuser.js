bcbd13b281ab539ea12adfa1151f375f698c1682
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-11 01:18:46 -0500

    Update owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -7,9 +7,9 @@ if (m.isGroup) who = m.mentionedJid[0]
 else who = m.chat
 if (!who) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 @𝚝𝚊𝚐 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*'
 let users = global.db.data.users
-let username = conn.getName(who)
+let name = await conn.getName(m.sender)
 users[who].banned = false
-conn.reply(m.chat, `     ❗ *USUARIO DESBANEADO* ❗\n\n┌──────────────────────\n\n*▢ Acceso al BOT.*\n*▢ Grupos y chat pv.*\n*▢ Disfrute del BOT:*\n    ${username}. \n\n└──────────────────────\n\n*•ATTE: BOT ${gt}*`, m)
+conn.reply(m.chat, `     ❗ *USUARIO DESBLOQUEADO* ❗\n\n┌──────────────────────\n\n*▢ Acceso al BOT.*\n*▢ Grupos y chat pv.*\n*▢ Disfrute del BOT:*\n    ${name}. \n\n└──────────────────────\n\n*•ATTE: BOT ${gt}*`, m)
 }
 handler.help = ['unblock']
 handler.tags = ['owner']
