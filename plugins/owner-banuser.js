8df64c0bd644590b12bdf2af62aece3f29e90088
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-10 22:39:50 -0500

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -8,10 +8,11 @@ else who = m.chat
 if (!who) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 @𝚝𝚊𝚐 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*'
 let users = global.db.data.users
 users[who].banned = true
-conn.reply(m.chat, `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙵𝚄𝙴 𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾*\n*—◉ 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙽𝙾 𝙿𝙾𝙳𝚁𝙰 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙱𝙾𝚃 𝙷𝙰𝚂𝚃𝙰 𝚀𝚄𝙴 𝚂𝙴𝙰 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝙳𝙾*`, m)
+let name = await conn.getName(m.sender)
+conn.reply(m.chat, `        *❗ USUARIO BANEADO ❗*\n\n┌──────────────────────\n\n*▢ Registrado en blocklist.*\n*▢ No accede a comandos.*\n*▢ BOT ignora a :*\n    ${name}. \n\n└──────────────────────\n\n*•ATTE: 𝗕𝗢𝗧 ${gt}*`, m)
 }
 handler.help = ['banuser']
 handler.tags = ['owner']
-handler.command = /^banuser$/i
+handler.command = /^block$/i
 handler.rowner = true
 export default handler
