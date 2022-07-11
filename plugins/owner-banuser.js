b683c296d372221edd7d7ceda55636f8ac14fbe6
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-10 22:34:26 -0500

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,4 +1,6 @@
 let handler = async (m, { conn, text}) => {
+await conn.sendMessage(m.chat, { react: { text: '❎', key: m.key } })  
+await m.reply(global.wait)
 if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 @𝚝𝚊𝚐 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*'
 let who
 if (m.isGroup) who = m.mentionedJid[0]
