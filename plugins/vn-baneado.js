3a4c514194393637f7b9d68ce8e5b4bbe0282340
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-11 17:20:35 -0500

    Update vn-baneado.js

--- plugins/vn-baneado.js
+++ plugins/vn-baneado.js
@@ -2,6 +2,7 @@ import util from 'util'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
+await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } })  
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/baneado.mp3'
 conn.sendFile(m.chat, vn, 'baneado.mp3', null, m, true, {
