dfbd79c8d9a058aab1902ee8bb594e7f4202e83a
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-10 20:45:31 -0500

    Update owner-unban.js

--- plugins/owner-unban.js
+++ plugins/owner-unban.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, text}) => {
 }
 handler.help = ['ban']
 handler.tags = ['owner']
-handler.command = /^unblock$/i
+handler.command = /^unban$/i
 handler.owner = false
 handler.premium = true
 
