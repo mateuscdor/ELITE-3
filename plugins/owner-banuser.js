ba298fbb2ef1143a5ea450232f9c854b5a9db5fc
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-11 18:33:03 -0500

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,5 +1,7 @@
-
-let handler = async (m, { conn, text, command, usedPrefix }) => {
+let handler = async (m, { conn, text}) => {
+let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } })  
+await m.reply(global.wait)
 if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 @𝚝𝚊𝚐 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*'
 let who
 if (m.isGroup) who = m.mentionedJid[0]
@@ -7,12 +9,10 @@ else who = m.chat
 if (!who) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 @𝚝𝚊𝚐 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*'
 let users = global.db.data.users
 users[who].banned = true
-let username = conn.getName(who)
-conn.reply(m.chat, `        ❗ *USUARIO BLOQUEADO* ❗\n\n┌──────────────────────\n\n*▢ Acceso al BOT.*\n*▢ Grupos y chat pv.*\n*▢ Disfrute del BOT:*\n    ${username}. \n\n└──────────────────────\n\n*•ATTE: BOT ${gt}*`, m)
+conn.reply(m.chat, `      ❗ *USUARIO BLOQUEADO* ❗\n\n┌──────────────────────\n\n*▢ Acceso al BOT.*\n*▢ Grupos y chat pv.*\n*▢ Disfrute del BOT:*\n    |. \n\n└──────────────────────\n\n*•ATTE: BOT ${gt}*`, m)
 }
-handler.help = ['ban']
+handler.help = ['banuser']
 handler.tags = ['owner']
 handler.command = /^block$/i
-handler.group = true
-handler.admin = true 
+handler.rowner = true
 export default handler
