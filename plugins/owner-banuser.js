e148a0c15fbd84e043858b24f420bacb6effb5f1
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-10 20:36:35 -0500

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -7,7 +7,7 @@ if (!who) throw `❗𝗘𝗧𝗜𝗤𝗨𝗘𝗧𝗔𝗥 𝗔 𝗔𝗟𝗚𝗨�
 let users = global.db.data.users
 
 let username = conn.getName(who)
-conn.reply(m.chat, `    *❗ USUARIO BANEADO ❗*\n\n*▢ Registrado en blocklist.*\n*▢ No accede a comandos.*\n*▢ BOT ignora a :*\n      ${username}. \n\n*•ATTE: 𝗕𝗢𝗧 ${gt}*`, m)
+conn.reply(m.chat, `    *❗ USUARIO BANEADO ❗*\n\n┌──────────────────────\n\n*▢ Registrado en blocklist.*\n*▢ No accede a comandos.*\n*▢ BOT ignora a :*\n   ${username}. \n\n└──────────────────────\n\n*•ATTE: 𝗕𝗢𝗧 ${gt}*`, m)
 }
 handler.help = ['ban']
 handler.tags = ['owner']
