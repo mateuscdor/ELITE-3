25f25dc50c9f93a6c7ac7166079e62d0b66c5089
MASTER-VIP-ADM <107499757+MASTER-VIP-ADM@users.noreply.github.com>
2022-07-11 12:38:04 -0500

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,3 +1,4 @@
+
 let handler = async (m, { conn, text}) => {
 await conn.sendMessage(m.chat, { react: { text: '❎', key: m.key } })  
 await m.reply(global.wait)
@@ -7,9 +8,9 @@ if (m.isGroup) who = m.mentionedJid[0]
 else who = m.chat
 if (!who) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 @𝚝𝚊𝚐 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*'
 let users = global.db.data.users
+let username = conn.getName(who)
 users[who].banned = true
-let name = await conn.getName(m.sender)
-conn.reply(m.chat, `        *❗ USUARIO BANEADO ❗*\n\n┌──────────────────────\n\n*▢ Registrado en blocklist.*\n*▢ No accede a comandos.*\n*▢ BOT ignora a :*\n    ${name}. \n\n└──────────────────────\n\n*•ATTE: 𝗕𝗢𝗧 ${gt}*`, m)
+conn.reply(m.chat, `        *❗ USUARIO BANEADO ❗*\n\n┌──────────────────────\n\n*▢ Registrado en blocklist.*\n*▢ No accede a comandos.*\n*▢ BOT ignora a :*\n    ${username}. \n\n└──────────────────────\n\n*•ATTE: 𝗕𝗢𝗧 ${gt}*`, m)
 }
 handler.help = ['banuser']
 handler.tags = ['owner']
